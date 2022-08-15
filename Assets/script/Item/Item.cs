d7910c9863db0d990ae04da257b29c1148c13a89
DirecterX <Pusid.Suakong@gmail.com>
2022-08-15 15:54:39 +0700

    Update Equipment System
    Update Equipment System
    -Create Equipment.cs
    -Create EquipmentManager.cs
    
    To do Next
    -Create UI for Equipment

--- Assets/script/Item/Item.cs
+++ Assets/script/Item/Item.cs
@@ -18,6 +18,11 @@ public class Item : ScriptableObject
 
         Debug.Log("Using " + name);
     }
+
+    public void RemoveFromInventory()
+    {
+        Inventory.instance.RemoveItem(this);
+    }
 }
 
 
