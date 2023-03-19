4d8b2562e596de530242f8e365516688a8d1285e
SarayutPech <pech13781@gmail.com>
2023-03-19 21:27:21 +0700

    เปลี่ยนจากยืนรอหน้าประตู -> กด F เพื่อ Interact
    กล่องสมบัติในเกมและประตูวาร์ป

--- Assets/script/map/Castle/Room/warpDoor/RewardChest.cs
+++ Assets/script/map/Castle/Room/warpDoor/RewardChest.cs
@@ -3,16 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RewardChest : MonoBehaviour
+public class RewardChest : Interactable
 {
-    public float x, y;
-
-    public LayerMask player;
-    public GameObject slider;
-    private float progressBar = 0;
-    public float maxProgressValue;
     public Animator anim;
-
     public bool notOpen = true;
     public GameObject key_rawImg;
 
@@ -20,36 +13,8 @@ public class RewardChest : MonoBehaviour
 
     private void Awake()
     {
-        slider.GetComponent<Slider>().maxValue = maxProgressValue;
         anim = GetComponent<Animator>();
         levelManagerParameter = GameObject.Find("level manager").GetComponent<LevelManagerParameter>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Collider2D playerCol = Physics2D.OverlapBox(transform.position, new Vector2(x, y), 0f, player);
-        if (playerCol && notOpen)
-        {
-            slider.SetActive(true);
-            if (progressBar < maxProgressValue)
-            {
-                progressBar += Time.deltaTime;
-                slider.GetComponent<Slider>().value = progressBar;
-            }
-            else
-            {
-                notOpen = false;
-                slider.SetActive(false);
-                // Do something
-                giveReward();
-            }
-        }
-        else
-        {
-            slider.SetActive(false);
-            progressBar = 0;
-        }
     }
 
     private void giveReward()
@@ -57,13 +22,15 @@ public class RewardChest : MonoBehaviour
         // give key
         anim.SetTrigger("Open");
         levelManagerParameter.givekeys();
-
         // give Item
     }
 
-    private void OnDrawGizmos()
+    
+    public override void Interact()
     {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireCube(transform.position, new Vector2(x, y));
+        notOpen = false;
+        // Do something
+        giveReward();
+        
     }
 }
--- Assets/script/map/Castle/Room/warpDoor/warpDoor.cs
+++ Assets/script/map/Castle/Room/warpDoor/warpDoor.cs
@@ -4,85 +4,46 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class warpDoor : MonoBehaviour
+public class warpDoor : Interactable
 {
-    public float x, y;
-    public LayerMask player;
-    public GameObject slider;
-    private float progressBar = 0;
-
     private GameState gameState;
     private Player_Status ps;
     public Vector3 Player_spawn;
-
-    public float maxProgressValue;
 
     private LevelManagerParameter levelManagerParameter;
     private void Awake()
     {
         gameState = GameObject.Find("level manager").GetComponent<GameState>();
         ps = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Status>();
-        slider.GetComponent<Slider>().maxValue = maxProgressValue;
         levelManagerParameter = GameObject.Find("level manager").GetComponent<LevelManagerParameter>();
     }
-
-    private void Update()
-    {
-        Collider2D playerCol = Physics2D.OverlapBox(transform.position, new Vector2(x, y), 0f, player);
-        if (playerCol)
-        {
-            slider.SetActive(true);
-            if(progressBar < maxProgressValue)
-            {
-                progressBar += Time.deltaTime;
-                slider.GetComponent<Slider>().value = progressBar;
-            }else if(progressBar >= maxProgressValue && levelManagerParameter.keys == true)
-            {
-                ps.floorLevel += 1;
-                if (GameObject.Find("Warp_Door_" + ps.floorLevel)) // มีชั้นต่อไป
-                {
-                    gameState.move(GameObject.Find("Warp_Door_" + ps.floorLevel).transform.position + Player_spawn);
-
-                    //Update Game Result
-                    ResultScreen result = GameObject.Find("GameManager").GetComponent<ResultScreen>();
-                    result.Increase_Floor_Reach();
-
-                }
-                else // อยู่บนยอดแล้ว 
-                {
-                    // ส่งไปซีนต่อไป
-                    goToBossRoom();
-                }
-                progressBar = 0;
-                levelManagerParameter.usekeys();
-            }
-            else
-            {
-                slider.SetActive(false);
-            }
-        }
-        else
-        {
-            slider.SetActive(false);
-            progressBar = 0;
-        }
-    }
-
-    public void interact()
-    {
-        Debug.Log(gameObject.name);
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireCube(transform.position,new Vector2(x, y));
-    }
-
 
     private void goToBossRoom()
     {
         SceneManager.LoadScene("Boss_Castle");
         GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(-7,-1,-5);
+    }
+
+    public override void Interact()
+    {
+        if(levelManagerParameter.keys == true)
+        {
+            ps.floorLevel += 1;
+            if (GameObject.Find("Warp_Door_" + ps.floorLevel)) // มีชั้นต่อไป
+            {
+                gameState.move(GameObject.Find("Warp_Door_" + ps.floorLevel).transform.position + Player_spawn);
+
+                //Update Game Result
+                ResultScreen result = GameObject.Find("GameManager").GetComponent<ResultScreen>();
+                result.Increase_Floor_Reach();
+
+            }
+            else // อยู่บนยอดแล้ว 
+            {
+                // ส่งไปซีนต่อไป
+                goToBossRoom();
+            }
+            levelManagerParameter.usekeys();
+        }
     }
 }
