18942260fe2be038454b10f61afae35d5f5c3077
DirecterX <Pusid.Suakong@gmail.com>
2023-03-21 15:36:24 +0700

    critical system
    Implement critical hit when attack

--- Assets/script/CheckEnemyHit.cs
+++ Assets/script/CheckEnemyHit.cs
@@ -24,6 +24,8 @@ public class CheckEnemyHit : MonoBehaviour
     public void CheckHit()
     {
         Collider2D[] enemyHit = Physics2D.OverlapBoxAll(playerAttackHitbox.position, playerHitbox, 0, enemyLayer);
+
+        int damage = CheckCriticalHit(charaStat.attack.GetValue(), charaStat.critRate.GetValue(), charaStat.critDamage.GetValue());
         try
         {
             foreach (Collider2D i in enemyHit)
@@ -32,10 +34,10 @@ public class CheckEnemyHit : MonoBehaviour
                 //i.GetComponent<Animator>().SetTrigger("getHit");
 
                 if (i.GetComponent<BossStat>())
-                    i.GetComponent<BossStat>().setHp(charaStat.attack.GetValue());
+                    i.GetComponent<BossStat>().setHp(damage);
                 else if (i.GetComponent<EnemyStat>())
-                    i.GetComponent<EnemyStat>().setHp(charaStat.attack.GetValue());
-                Debug.Log("Attack Enemy " + i + " Enemy Take" + charaStat.attack.GetValue() + "Damage.");
+                    i.GetComponent<EnemyStat>().setHp(damage);
+                Debug.Log("Attack Enemy " + i + " Enemy Take" + damage + "Damage.");
             }
         }
         catch
@@ -47,5 +49,26 @@ public class CheckEnemyHit : MonoBehaviour
     public float ScaleX()
     {
         return transform.root.gameObject.transform.localScale.x;
+    }
+
+    public int CheckCriticalHit(int attack , int crit_rate , int crit_dmg)
+    {
+        float damageDeal;
+
+        float critChance = (float)crit_rate/100;
+        float critDmg = (float)crit_dmg / 100;
+
+        float randValue = Random.value;
+        if (randValue < critChance)
+        {
+            damageDeal = attack * (1 + critDmg);
+            Debug.Log("Critical hit!");
+        }
+        else
+        {
+            damageDeal = (float)attack;
+        }
+
+        return (int)damageDeal;
     }
 }
